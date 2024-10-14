4808ee1c0ef87daf20999d0bd96d23c852478627
biztyogesh <yogesh.dhiman@biztecno.net>
2024-10-14 15:47:14 +0530

    Basic Routing

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,12 +1,16 @@
+"use client";
 import React from "react";
 import styles from "./page.module.css";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 function Login() {
+  const router = useRouter();
   return (
     <div>
       <h1>Login Page</h1>
       <Link href={"./"}>Go to home page</Link>
+      <button onClick={() => router.push("./")}>about page</button>
     </div>
   );
 }
